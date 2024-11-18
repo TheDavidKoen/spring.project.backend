3381b438c1c4805042bfe828bd1cdac350d657e1
David Koen <davidodawie@gmail.com>
2024-11-18 11:42:33 +0200

    published spring backend

---
+++ spring.project/src/main/java/code/college/spring/spring/project/movies/MovieController.java
@@ -0,0 +1,38 @@
+package code.college.spring.spring.project.movies;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@Tag(name = "Movies' APIs", description = "Here are all the controllers for the movies' data.")
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+
+    @Autowired
+    private MovieService service;
+
+    @Operation(
+            summary = "Retrieves all movies.",
+            description = "Sends a GET request that lists all the movies.",
+            tags = { "Movies", "Get Requests" })
+    @GetMapping
+    public ResponseEntity<List<Movie>> getMovies() {
+        return new ResponseEntity<List<Movie>>(service.findAllMovies(), HttpStatus.OK);
+    }
+
+    @Operation(
+            summary = "Retrieves an individual movie.",
+            description = "Sends a GET request that lists a single movie.",
+            tags = { "Movies", "Get Requests" })
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId){
+        return new ResponseEntity<Optional<Movie>>(service.findMovieByImdbId(imdbId), HttpStatus.OK);
+    }
+}
---
+++ spring.project/src/main/java/code/college/spring/spring/project/movies/ReviewController.java
@@ -0,0 +1,29 @@
+package code.college.spring.spring.project.movies;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@Tag(name = "Reviews' APIs", description = "Here are all the controllers for the reviews' data.")
+@RestController
+@RequestMapping("/reviews")
+public class ReviewController {
+    @Autowired
+    private ReviewService service;
+
+    @Operation(
+            summary = "Posts a review.",
+            description = "Sends a POST request that creates a review.",
+            tags = { "Reviews", "Post Request" })
+    @PostMapping()
+    public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload) {
+
+        return new ResponseEntity<Review>(service.createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.OK);
+    }
+
+}
